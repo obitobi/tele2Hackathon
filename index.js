71dbb57d00505e411491611b1423144cb41107c1
andrey.zhukov <andrey.zhukov@reunico.com>
2020-08-22 16:37:08 +0300

    js beginning

---
+++ index.js
@@ -0,0 +1,18 @@
+const questions = [
+  {
+    topic: 'Первое сообщение',
+    content: 'Hei!'
+  },
+  {
+    topic: 'Gb',
+    content: 'Hei!'
+  },
+  {
+    topic: 'Mins',
+    content: 'Hei!'
+  },
+  {
+    topic: 'Social',
+    content: 'Hei!'
+  },
+]
