71d909039e1d9d21acb7206c9b8050522c78799f
andrey.zhukov <andrey.zhukov@reunico.com>
2020-08-22 17:31:53 +0300

    feat: js chat-bot

--- index.js
+++ index.js
@@ -16,3 +16,45 @@ const questions = [
     content: 'Hei!'
   },
 ]
+
+const fBtn = document.querySelector('#first');
+const sBtn = document.querySelector('#second');
+const tBtn = document.querySelector('#third');
+const forthBtn = document.querySelector('#forth');
+const firstAnsw = document.querySelector('#firstAnsw');
+
+const qaTemplate = document.querySelector('#dialog__element').content;
+const qaList = document.querySelector('.popup__dialog');
+
+function createNewDialogPair(text) {
+  const qaEl = qaTemplate.cloneNode(true);
+  qaEl.querySelector('#answ').classList.add('invisible');
+  qaEl.querySelector('#question').textContent = text;
+  return qaEl;
+}
+
+function renderCard(qaEl) {
+  qaList.append(qaEl);
+}
+
+function initPage() {
+  tBtn.classList.add('invisible');
+  fBtn.addEventListener('click', showHelloAnsw);
+  forthBtn.classList.add('invisible');
+  fBtn.textContent = 'Hello!';
+  sBtn.textContent = 'Goodbye!';
+  firstAnsw.classList.add('invisible');
+}
+
+function showHelloAnsw() {
+  firstAnsw.classList.remove('invisible');
+  tBtn.classList.remove('invisible');
+  forthBtn.classList.remove('invisible');
+  fBtn.textContent = 'Много';
+  sBtn.textContent = 'Средне';
+  tBtn.addEventListener('click', function () {
+    renderCard(createNewDialogPair('Hello, Drone, kak dela? Skok Gb?'));
+  });
+}
+
+initPage();
