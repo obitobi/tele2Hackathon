19ccab93b1d211c96ef9b04e96db4804d7ad792d
Elizaveta Timonina <elizaveta1207@gmail.com>
2020-08-23 08:50:49 +0300

    feat: make unlimited choice

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const iconsSocial = [{
     link: "./images/ok.svg",
+    isCheked: false,
   },
   {
     link: "./images/ok.svg",
+    isCheked: false,
   },
   {
     link: "./images/ok.svg",
+    isCheked: false,
   },
   {
     link: "./images/ok.svg",
+    isCheked: false,
   }
 ];
 
@@ -92,13 +96,24 @@ unlimitedItem.forEach((item) => item.addEventListener('click', () => {
 }));
 
 
-function addIcon(link) {
-  const icon = iconTemplate.querySelector('.unlimited__button').cloneNode(true);
+function addIcon(item) {
+  const icon = iconTemplate.querySelector('.popup__social').cloneNode(true);
 
-  icon.src = link;
-  // обработчик события для нажатия лайка
-  icon.addEventListener("click", function (evt) {
-    evt.target.classList.toggle("unlimited__button_active");
+  icon.src = item.link;
+
+  if (item.isCheked == true) {
+    icon.classList.add("popup__social-colored");
+  }
+
+  icon.addEventListener("click", function () {
+    if (!icon.classList.contains("popup__social-colored")) {
+      icon.classList.add("popup__social-colored");
+      item.isCheked = true;
+    } else {
+      icon.classList.remove("popup__social-colored");
+      item.isCheked = false;
+    }
+    // icon.classList.toggle("popup__social-colored");
   });
 
   return icon;
@@ -106,7 +121,7 @@ function addIcon(link) {
 
 
 function showIcons(item) {
-  const allUnlimitedButtons = Array.from(unlimitedButtons.querySelectorAll('.unlimited__button'));
+  const allUnlimitedButtons = Array.from(unlimitedButtons.querySelectorAll('.popup__social'));
   allUnlimitedButtons.forEach((btn) => {
     btn.remove();
   });
@@ -114,27 +129,27 @@ function showIcons(item) {
   if (item.classList.contains('unlimited__item-social')) {
     iconsSocial.forEach((item) => {
       // передаем селектор шаблона карточки в класс Card
-      unlimitedButtons.append(addIcon(item.link));
+      unlimitedButtons.append(addIcon(item));
     });
   } else if (item.classList.contains('unlimited__item-messenger')) {
     iconsMessenger.forEach((item) => {
       // передаем селектор шаблона карточки в класс Card
-      unlimitedButtons.append(addIcon(item.link));
+      unlimitedButtons.append(addIcon(item));
     });
   } else if (item.classList.contains('unlimited__item-music')) {
     iconsMusic.forEach((item) => {
       // передаем селектор шаблона карточки в класс Card
-      unlimitedButtons.append(addIcon(item.link));
+      unlimitedButtons.append(addIcon(item));
     });
   } else if (item.classList.contains('unlimited__item-video')) {
     iconsVideo.forEach((item) => {
       // передаем селектор шаблона карточки в класс Card
-      unlimitedButtons.append(addIcon(item.link));
+      unlimitedButtons.append(addIcon(item));
     });
   } else if (item.classList.contains('unlimited__item-other')) {
     iconsOther.forEach((item) => {
       // передаем селектор шаблона карточки в класс Card
-      unlimitedButtons.append(addIcon(item.link));
+      unlimitedButtons.append(addIcon(item));
     });
   }
 
